8793153b76f8d02c95f549eb5f0861cb80efbad6
sumankanta <sumankpadhan@gmail.com>
2026-01-30 16:02:23 +0530

    feat: create initial folder structure for React learning project

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+
+const App = () => {
+  return (
+    <>
+      <div id="div1">
+      <h1>Suman</h1>
+      <h2>Suman</h2>
+    </div>
+    <div id="div2">
+      <h1>Suman</h1>
+      <h2>Suman</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
