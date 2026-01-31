20855d7b8d4fe560c90693d33cd671ce7631927b
sumankanta <sumankpadhan@gmail.com>
2026-01-31 23:22:26 +0530

    style: integrate Tailwind CSS into React project

---
+++ 06-Tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="h-screen w-full bg-gray-900 justify-center items-center flex ">
+      <h1 class="text-3xl font-bold underline text-amber-50 text-decoration-none">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
