0a59a7d6aef64579d90d021ce8e24be8bf8e88a9
sumankanta <sumankpadhan@gmail.com>
2026-02-05 23:00:42 +0530

    feat(react): implement examples with useState, advanced hooks, and form handling

---
+++ 10-useState-advance/src/App.jsx
@@ -0,0 +1,48 @@
+// import React, { useState } from 'react'
+
+// const App = () => {
+
+//   const [num, setnum] = useState(10)
+
+//   const buttonClick = () =>{
+//     console.log('Current value ' + num);
+//     setnum(num+1);
+//   }
+
+//   return (
+//     <>
+//     <h1>{num}</h1>
+//     <button onClick={buttonClick}>Click</button>
+//     </>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [data, setdata] = useState({user:'Suman', age:23})
+
+  const btnClicked = () => {
+    // const newData = {...data}
+    // newData.user = 'Sumankanta'
+    // setdata(newData)
+
+    // newData.age = 24
+    // setdata(newData)
+
+    setdata(prev => ({...data, age:24}))
+    }
+
+
+  return (
+    <>
+    <h1>{data.user}, {data.age}</h1>
+    <button onClick={btnClicked}>Click</button>
+    </>
+  )
+}
+
+export default App
